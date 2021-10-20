d4f8a3f9ea5e0bf24f9b54b91b8de5273f834c3d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-10-20 18:38:31 +0300

    init

--- lib/math operations/my_math.dart
+++ lib/math operations/my_math.dart
@@ -1,5 +1,4 @@
 class MathOperation {
-
   double addition(double a1, double a2) {
     return a1 + a2;
   }
@@ -13,14 +12,13 @@ class MathOperation {
   }
 
   double division(double a1, double a2) {
-    if(a2 != 0) {
-      return a1 / a2;
+    if (a2 == 0) {
+      return 1010;
     }
-    return 000;
+    return a1 / a2;
   }
 
-    double percent(double a1, double a2) {
+  double percent(double a1, double a2) {
     return a1 % a2;
   }
-
 }
